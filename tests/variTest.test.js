d5d91d6181a1551512a5ba614136612c4fde044f
CarloDettori <dettoric07@gmail.com>
2025-05-22 16:44:34 +0200

    snack 2

--- tests/variTest.test.js
+++ tests/variTest.test.js
@@ -1,10 +1,63 @@
 
-
-function inizialiDi(Nome, Cognome) {
-    return Nome.charAt(0).toUpperCase() + Cognome.charAt(0).toUpperCase()
+//SNACK 1
+function inizialiDi(nome, cognome) {
+    return nome.charAt(0).toUpperCase() + cognome.charAt(0).toUpperCase()
 }
 
 test("La funzione getInitials restituisce le iniziali di un nome completo.", () => {
     const iniziali = inizialiDi("Carlo", "Dettori")
     expect(iniziali).toBe("CD")
 })
+
+//SNACK 2
+function createSlug(stringa) {
+    return stringa.toLowerCase()
+}
+
+test("La funzione createSlug restituisce una stringa in lowercase.", () => {
+    const slug = createSlug("Dettori")
+    expect(slug).toBe("dettori")
+})
+
+
+//SNACK 2
+
+
+
+//SNACK 2
+
+
+
+//SNACK 2
+
+
+
+//SNACK 2
+
+
+
+//SNACK 2
+
+
+
+//SNACK 2
+
+
+
+//SNACK 2
+
+
+
+//SNACK 2
+
+
+//SNACK 2
+
+
+//SNACK 2
+
+
+//SNACK 2
+
+
+//SNACK 2
